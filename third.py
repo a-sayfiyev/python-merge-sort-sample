74fe964b9389b8fe589521b86fe42d2c599ade70
a-sayfiyev <akobirsayfiyev27@gmail.com>
2023-11-22 12:36:10 +0500

    first commit

---
+++ third.py
@@ -0,0 +1,41 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    left_half = merge_sort(left_half)
+    right_half = merge_sort(right_half)
+
+    return merge(left_half, right_half)
+
+
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+
+    return result
+
+
+# can be used any numbers
+left_index = [0, 2, 7, 5]
+right_index = [3, 4]
+
+combined_index = left_index + right_index
+
+sorted_index = merge_sort(combined_index)
+
+print(sorted_index)
